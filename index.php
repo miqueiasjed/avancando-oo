3d5db943880e64ef04e0af87747f21d291ab577b
Miqueias Mesquita <miqueiasjedmike@gmail.com>
2016-09-22 15:57:46 -0300

    Index e Listando Registros com PDO

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
 
-echo "Testando PHP";
+$db = new \PDO("mysql:host=localhost;dbname=avancando-oo","root","");
+
+$query = "select * from products";
+
+$stmt = $db->prepare($query);
+$stmt->execute();
+
+$list = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+var_dump($list);
