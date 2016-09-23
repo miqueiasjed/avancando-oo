19049e69880e8deeb6f49929d5e4fc9232fc248d
Miqueias Mesquita <miqueias_jed@hotmail.com>
2016-09-23 00:28:10 -0300

    Corrigindo Pimple

--- class_products/public/index.php
+++ class_products/public/index.php
@@ -1,3 +1,21 @@
 <?php 
 
 require_once "../vendor/autoload.php";
+
+use Pimple\Container;
+
+$container = new Container();
+
+$container['conn'] = function()
+{
+    return new \Source\Conn("mysql:host=localhost;dbname=avancando-oo","root","");
+};
+
+$container['product'] = function($c)
+{
+    return new Source\Product($c['conn']);
+};
+
+$list = $container['product']->listar();
+
+var_dump($list);
